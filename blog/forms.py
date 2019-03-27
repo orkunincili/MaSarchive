478ceeb13003a34cab990d28c2d670fe6afa9ca7
orkunincili <orkunincili0@gmail.com>
2019-03-27 18:50:23 +0300

    Added blog forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,12 +1,24 @@
 from django import forms
 from .models import add_multiple
+from .models import Diary
 
 
 
 
 
+class DiaryForm(forms.ModelForm):
 
 
+    class Meta:
+
+        model=Diary
+
+        fields=[
+
+            "title",
+            "content",
+            "date",
+        ]
 
 class add_multipleForm(forms.ModelForm):
 
