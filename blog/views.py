a94329859734b0f82a265ad0346cf72c297eb7a2
orkunincili <orkunincili0@gmail.com>
2019-03-27 18:51:28 +0300

    Added blog views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,58 @@
+from django.shortcuts import render
+from .models import Diary
+from home.models import User
+from .forms import add_multipleForm,DiaryForm
+import os,magic,webbrowser
 
 
+
+def book_index(request):
+   diary=Diary.objects.all()
+   user = User.objects.all()
+
+   context={
+       "diary":diary,
+       "user":user,
+   }
+   return render(request,"blog/blog_index.html",context)
+def book_create(request):
+    form = DiaryForm(request.POST or None)
+    user = User.objects.all()
+    if form.is_valid():
+        diary = form.save(commit=False)
+
+        diary.save()
+
+
+
+    context = {
+        'form': form,
+        'user': user,
+    }
+
+    return render(request, "blog/blog_form.html", context)
+
+
+def book_update(request,id):
+    book=Diary.Objects.get(id=id)
+    return HttpResponse("update")
+
+
+def book_delete(request):
+    return HttpResponse("delete")
+
+def book_detail(request,id):
+    diary = Diary.objects.get(id=id)
+    user = User.objects.all()
+
+    context = {
+        "diaryD": diary,
+        "user": user,
+    }
+
+    return render(request, "blog/blog_detail.html", context)
+
+
+
+    return render(request,"blog/blog_index.html")
 
