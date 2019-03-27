ae73b9539a6532a950a081217d09d0c91abc15de
orkunincili <orkunincili0@gmail.com>
2019-03-27 18:51:03 +0300

    Added blog urls

--- blog/urls.py
+++ blog/urls.py
@@ -6,9 +6,19 @@ app_name = "blog"
 urlpatterns = [
 
 
+    url(r'^blog_index/$',book_index),
+    url(r'^(?P<id>\d+)/$',book_detail,name="blog_detail"),
+    url(r'^blog_create/$', book_create),
+    url(r'^blog_update/$',book_update),
+    url(r'^blog_delete/$', book_delete),
 
 
 
 
 
 ]
+
+
+
+
+
