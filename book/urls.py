667e32c74ab1e3a345d436055bff5f444b3a9f68
orkunincili <orkunincili0@gmail.com>
2019-04-17 21:50:43 +0300

    Edited urls.py and views.py

--- book/urls.py
+++ book/urls.py
@@ -2,17 +2,16 @@ from django.conf.urls import url
 from .views import *
 
 app_name = "book"
+
 urlpatterns = [
 
 
-    url(r'^book_index/$',book_index),
-    url(r'^(?P<id>\d+)/$',book_detail,name="book_detail"),
-    url(r'^book_create/$', book_create),
-    url(r'^book_update/$',book_update),
-    url(r'^book_delete/$', book_delete),
-    url(r'^add_book/$',add_multiple_book),
-
-
+        url(r'^book_index/$',book_index),
+        url(r'^(?P<id>\d+)/$',book_detail,name="book_detail"),
+        url(r'^book_create/$', book_create),
+        url(r'^book_update/$',book_update),
+        url(r'^book_delete/$', book_delete),
+        url(r'^add_book/$',add_multiple_book),
 
 
 ]
--- book/views.py
+++ book/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 from .models import Book
 from home.models import User
@@ -10,47 +10,54 @@ import os,magic,webbrowser
 def book_index(request):
    books=Book.objects.all()
    user = User.objects.all()
-   bookR=[]
-   bookNR=[]
-   favorite_books=[]
-   for i in books:
-       if(i.favorite_book=="F"):
-           favorite_books.append(i)
-       elif(i.read=='R'):
-           bookR.append(i)
-       else:
-           bookNR.append(i)
-   len_fav=len(favorite_books)
-   len_R=len(bookR)
-   len_nR=len(bookNR)
-   context={
-       "books":books,
-       "bookR":bookR,
-       "bookNR":bookNR,
-       "user": user,
-       "fav_book":favorite_books,
-       "len_fav":len_fav,
-       "len_R":len_R,
-       "len_nR":len_nR
-    }
-   return render(request,"books/book_index.html",context)
+   if user[0].select_book=='E':
+       bookR=[]
+       bookNR=[]
+       favorite_books=[]
+
+       for i in books:
+           if(i.favorite_book=="F"):
+               favorite_books.append(i)
+           elif(i.read=='R'):
+               bookR.append(i)
+           else:
+               bookNR.append(i)
+       len_fav=len(favorite_books)
+       len_R=len(bookR)
+       len_nR=len(bookNR)
+       context={
+           "books":books,
+           "bookR":bookR,
+           "bookNR":bookNR,
+           "user": user,
+           "fav_book":favorite_books,
+           "len_fav":len_fav,
+           "len_R":len_R,
+           "len_nR":len_nR
+        }
+       return render(request,"books/book_index.html",context)
+   else:
+       return HttpResponse("Disable")
 def book_create(request):
-    form = BookForm(request.POST or None)
+
     user = User.objects.all()
-    if form.is_valid():
-        book = form.save(commit=False)
+    if user[0].select_book == 'E':
+        form = BookForm(request.POST or None)
+        if form.is_valid():
+            book = form.save(commit=False)
 
-        book.save()
+            book.save()
 
 
 
-    context = {
-        'form': form,
-        'user': user,
-    }
+        context = {
+            'form': form,
+            'user': user,
+        }
 
-    return render(request, "books/book_form.html", context)
-
+        return render(request, "books/book_form.html", context)
+    else:
+        return HttpResponse("App is disable")
 
 def book_update(request,id):
     book=Book.Objects.get(id=id)
@@ -61,52 +68,59 @@ def book_delete(request):
     return HttpResponse("delete")
 
 def book_detail(request,id):
-    book = Book.objects.get(id=id)
     user = User.objects.all()
-    webbrowser.open(book.book_path)
-    context = {
-        "bookD": book,
-        "user": user,
-    }
+    if user[0].select_book == 'E':
+        book = Book.objects.get(id=id)
+        user = User.objects.all()
+        webbrowser.open(book.book_path)
+        context = {
+            "bookD": book,
+            "user": user,
+        }
 
-    return render(request, "books/book_detail.html", context)
+        return render(request, "books/book_detail.html", context)
 
 
 
-    return render(request,"books/book_index.html")
-
+        return render(request,"books/book_index.html")
+    else:
+        return HttpResponse("App is disable")
 def add_multiple_book(request):
-    form = add_multipleForm(request.POST or None,request.FILES or None)
     user = User.objects.all()
-    file_list=[]
-    book_list = []
-    if form.is_valid():
-        path = form.save()
+    if user[0].select_book == 'E':
+        form = add_multipleForm(request.POST or None,request.FILES or None)
+        user = User.objects.all()
+        file_list=[]
+        book_list = []
+        if form.is_valid():
+            path = form.save()
 
-        for root, dirs, files in os.walk(path.path):
-            for file in files:
-                file_list.append(os.sep.join([root, file]))
+            for root, dirs, files in os.walk(path.path):
+                for file in files:
+                    file_list.append(os.sep.join([root, file]))
 
-        for i in range(len(file_list)):
+            for i in range(len(file_list)):
 
-            mime_type = magic.from_file(file_list[i], mime=True)
+                mime_type = magic.from_file(file_list[i], mime=True)
 
-            if mime_type.lower().endswith(('/pdf', '/epub+zip')) == True:
-                book_list.append(file_list[i].split("/")[-1])
+                if mime_type.lower().endswith(('/pdf', '/epub+zip')) == True:
+                    book_list.append(file_list[i].split("/")[-1])
 
-        for i in book_list:
-
-
-            Book.objects.create(book_name=i,book_path="file://"+path.path+"/"+i)
-
-
+            for i in book_list:
 
 
+                Book.objects.create(book_name=i,book_path="file://"+path.path+"/"+i)
 
 
-    context = {
-        'form': form,
-        'user': user,
-    }
 
-    return render(request, "books/add_multiple_book.html", context)
+
+
+
+        context = {
+            'form': form,
+            'user': user,
+        }
+
+        return render(request, "books/add_multiple_book.html", context)
+    else:
+        return HttpResponse("App is disable")
